5260ce0a209440dc4bb55fdb0270a80db5d4d0f1
Anastasia Leonova <hihikka22@gmail.com>
2026-07-13 19:58:18 +0200

    refactor(providers): remove duplicate gesture handler root

--- src/common/providers/AppProvider.tsx
+++ src/common/providers/AppProvider.tsx
@@ -1,5 +1,4 @@
 import type { ReactNode } from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import Toast from "react-native-toast-message";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
@@ -15,18 +14,16 @@ interface AppProviderProps {
 
 export default function AppProvider({ children }: AppProviderProps) {
   return (
-    <GestureHandlerRootView>
-      <KeyboardProvider>
-        <BottomSheetModalProvider>
-          <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-        </BottomSheetModalProvider>
-        <PortalHost />
-        <PortalHost name="dialog" />
-        <PortalHost name="popover" />
-        <PortalHost name="select" />
-        <PortalHost name="dropdown" />
-        <Toast config={toastConfig} />
-      </KeyboardProvider>
-    </GestureHandlerRootView>
+    <KeyboardProvider>
+      <BottomSheetModalProvider>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </BottomSheetModalProvider>
+      <PortalHost />
+      <PortalHost name="dialog" />
+      <PortalHost name="popover" />
+      <PortalHost name="select" />
+      <PortalHost name="dropdown" />
+      <Toast config={toastConfig} />
+    </KeyboardProvider>
   );
 }
